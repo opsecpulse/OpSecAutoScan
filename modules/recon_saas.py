21453c3a8eb051b9b148a312c1f9f7337ada3b1a
Alin Razvan <alnraz@protonmail.com>
2025-06-20 16:20:34 -0400

    message

--- modules/recon_saas.py
+++ modules/recon_saas.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 def saml_enum(domain):
     try:
@@ -11,7 +12,13 @@ def saml_enum(domain):
         return [f"Error: {e}"]
 
 def subdomain_enum(domain):
+    import re
+    def clean_html(raw):
+        cleanr = re.compile("<.*?>")
+        return re.sub(cleanr, "", raw)
     output = subprocess.getoutput(f"subfinder -d {domain}")
+    clean_output = clean_html(output)
+    clean_output = re.sub(r'\x1B\[[0-?]*[ -/]*[@-~]', '', output)
     with open("results/recon.txt", "w") as f:
-        f.write(output)
-    return output
+        f.write(clean_output)
+    return clean_output
